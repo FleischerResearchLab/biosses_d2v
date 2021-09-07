102ea9bfd7e0601c225b396e1c616e7df73b5006
Bao Nguyen <hoaibaonguyen1999@gmail.com>
2021-09-07 15:46:05 -0700

    add argument parser for different doc2vec parameters

--- biosses_d2v.py
+++ biosses_d2v.py
@@ -1,8 +1,10 @@
 import os
 import re
 import pickle
+import spacy
 import string
 import logging
+import argparse
 import multiprocessing
 from subprocess import run
 from numpy import array
@@ -37,7 +39,8 @@ class BIOSSESDataset:
     def get_scores(self):
         doc = Document(os.path.abspath("BIOSSES-Dataset/Annotator-Scores.docx"))
         table = doc.tables[0]
-        score_arr = array([[cell.text.strip() for cell in row.cells] for row in table.rows], dtype="i1")
+        score_arr = array([[cell.text.strip() for cell in row.cells] \
+                            for row in table.rows], dtype="i1")
         return score_arr[1:,1:]
 
 
@@ -46,6 +49,7 @@ class PMCSubsetCorpus:
         self.sub_pattern = sub_pattern #r"(\s+\(*(([À-ÿA-Za-z\s\-.,;&])+\s\(*(\d{4}[a-z]*)+\)*)+)|(\s+\[[\d\s+,;&\[\]]+\])"
         self.stopwords = stopwords
         self.corpus_paths = []
+        self.nlp = None
         if lemma:
             self.nlp = spacy.load('en_core_sci_sm', disable=['tok2vec','parser','ner']) 
 
@@ -79,10 +83,12 @@ class PMCSubsetCorpus:
             body = re.search('==== Body(.*)==== Refs', paper, re.DOTALL)
 
             if body is not None:
-                doc = body.group(1).split()
+                doc = body.group(1).lower().split()
+                print(self.nlp)
                 if self.nlp is not None:
                     doc = " ".join(doc)
-                    doc = [token.lemma_ for token in self.nlp(doc) if not token in self.stopwords]
+                    doc = [token.lemma_ for token in self.nlp(doc) \
+                            if not token in self.stopwords]
                 else:
                     doc = [token for token in doc if not token in self.stopwords]
                 return TaggedDocument(doc, [id])
@@ -119,22 +125,90 @@ class PMCSubsetCorpus:
                 print(tagged_doc)
                 doc_id += 1
         return doc_list
-        
+
+
+def run_doc2vec(lemma=False, logger=False, progress_per=1000, **kwargs):
+    if logger:
+        logging.basicConfig(format="%(asctime)s : %(levelname)s : %(message)s", 
+                            level=logging.INFO)
+    
+    corpus = PMCSubsetCorpus(lemma=lemma)
+    corpus.load()
+    corpus = corpus.get_list()
+
+    for key, value in kwargs.items():
+        print("{0} = {1}".format(key, value))
+    model = Doc2Vec(**kwargs)
+    model.build_vocab(corpus, progress_per=progress_per)
+    model.train(corpus, total_examples=model.corpus_count, epochs=model.epochs)
+    #return model
+
 
 def main():
-    train_corpus = PMCSubsetCorpus(stopwords=set(stopwords.words('english')))
-    train_corpus.load()
-    biodata = BIOSSESDataset()
-    biosent = biodata.get_sentences()
-    print(biosent[59])
-    print(train_corpus.corpus_paths[10000])
-    train_corpus = train_corpus.get_list()
 
-    # Train doc2vec
-    #logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-    model = Doc2Vec(vector_size=96, min_count=5, workers=multiprocessing.cpu_count())
-    model.build_vocab(train_corpus, progress_per=1000)
-    model.train(train_corpus, total_examples=model.corpus_count, epochs=1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v", "--vector_size",
+        type=int,
+        default=56,
+        help="Dimensionality of Doc2Vec embeddings."
+    )
+    parser.add_argument(
+        "-w", "--window",
+        type=int,
+        default=5,
+        help="Maximum distance between current and predicted word during training."
+    )
+    parser.add_argument(
+        "-a", "--alpha",
+        type=float,
+        default=0.025,
+        help="Initial learning rate."
+    )    
+    parser.add_argument(
+        "-m", "--min_count",
+        type=int,
+        default=5,
+        help="Minimum frequency of accepted words."
+    )
+    parser.add_argument(
+        "-l", "--lemma",
+        action="store_true",
+        help="Whether tokens are turned into lemmas for training."
+    )
+    parser.add_argument(
+        "-W", "--workers",
+        type=int,
+        default=3,
+        help="Number of worker threads used to train the model."
+    )
+    parser.add_argument(
+        "-e", "--epochs",
+        type=int,
+        default=10,
+        help="Number of iterations trained over the corpus."
+    )
+    parser.add_argument(
+        "-L", "--logger",
+        action="store_true",
+        help="Whether to show training progress."
+    )
+    parser.add_argument(
+        "-p", "--progress_per",
+        type=int,
+        default=1000,
+        help="Number of words processed before showing progress."
+    )
+    args, unknown = parser.parse_known_args()
+    run_doc2vec(vector_size=args.vector_size, 
+                window=args.window, 
+                alpha=args.alpha,
+                min_count=args.min_count, 
+                lemma=args.lemma, 
+                workers=args.workers,
+                epochs=args.epochs, 
+                logger=args.logger, 
+                progress_per=args.progress_per)
 
 
 if __name__ == "__main__":
