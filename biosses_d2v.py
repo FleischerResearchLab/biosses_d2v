45bbd1838bca30939fa4cc4f4e290eafbe475b71
Bao Nguyen <hoaibaonguyen1999@gmail.com>
2021-09-16 11:44:08 -0700

    optimize imports, format code and re-add get_list in BIOSSESDataset

--- biosses_d2v.py
+++ biosses_d2v.py
@@ -1,24 +1,26 @@
 import os
-import re
 from subprocess import run
-from smart_open import open
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
 
 class BIOSSESDataset:
     """
     Dataset class for the BIOSSES Benchmark.
     The BIOSSES dataset includes 100 biomedical sentence pairs from
-    the TAC 2014 Biomedical Summarization Track (https://tac.nist.gov/2014/BiomedSumm/) 
-    whose similarity was judged by 5 different human experts according to 
+    the TAC 2014 Biomedical Summarization Track (https://tac.nist.gov/2014/BiomedSumm/)
+    whose similarity was judged by 5 different human experts according to
     the SemEval 2012 Task 6 Guideline on a scale of 0-4.
     This class can prepare the BIOSSES dataset, both the sentence pairs
     and similarity scores, in array form.
     Source: https://tabilab.cmpe.boun.edu.tr/BIOSSES/DataSet.html.
     """
-    
+
+    import docx
+    import spacy
+    import scipy
+    from pandas import DataFrame
+
     def __init__(self):
-        """Downloads BIOSSES data if current directory does not contain it."""
+        """Downloads BIOSSES data if the current directory does not contain it."""
 
         if not os.path.isfile("BIOSSES-Dataset/Annotation-Pairs.docx") \
            or not os.path.isfile("BIOSSES-Dataset/Annotator-Scores.docx"):
@@ -26,39 +28,72 @@ class BIOSSESDataset:
                           BIOSSES/Downloads/BIOSSES-Dataset.rar"])
             run(["unrar", "x", "BIOSSES-Dataset.rar"])
 
-
-    def _get_array_from_doc(self, path):
+    def _get_df_from_doc(self, path):
         """Loads a table from a .doc/docx file into an array.
-      
+
         Args: 
           path: Filepath of the document.
 
         Returns:
-          Array obtained from the file. 
+          DataFrame obtained from the file. 
         """
 
-        from docx import Document
-        from numpy import array
-
-        doc = Document(os.path.abspath(path))
+        doc = self.docx.Document(path)
         table = doc.tables[0]
-        arr = array([[cell.text.strip() for cell in row.cells] \
-                           for row in table.rows])
-        return arr
+        df = self.DataFrame([[cell.text.strip() for cell in row.cells]
+                             for row in table.rows])
+        return df
 
- 
-    def get_sentence_pairs(self):
+    def get_sentence_df(self):
         """Gets the BIOSSES sentence pairs for benchmarking."""
 
-        sent_arr = self._get_array_from_doc("BIOSSES-Dataset/Annotation-Pairs.docx")
-        return sent_arr[1:,1:]
+        sent_df = self._get_df_from_doc(
+            "BIOSSES-Dataset/Annotation-Pairs.docx")
+        return sent_df.iloc[1:, 1:].rename(columns={1: "Sentence_1",
+                                                    2: "Sentence_2"})
 
-    
-    def get_annotator_scores(self):
+    def get_score_df(self):
         """Gets the BIOSSES similarity scores by expert annotators."""
 
-        score_arr = self._get_array_from_doc("BIOSSES-Dataset/Annotator-Scores.docx")
-        return score_arr[1:,1:].astype("i1")
+        score_df = self._get_df_from_doc(
+            "BIOSSES-Dataset/Annotator-Scores.docx")
+        return score_df.iloc[1:, 1:].astype("i1")
+
+    def benchmark_with_d2v(self, d2v_model=None, lemma=False, stopwords=[]):
+        """Benchmarks a Doc2Vec model with the BIOSSES Dataset. 
+        Returns the Pearson correlation with annotators' similarity scores 
+        and also the cosine similarity scores produced by the Doc2Vec model.
+        """
+
+        cosine = self.scipy.spatial.distance.cosine
+        pearsonr = self.scipy.stats.pearsonr
+        nlp = self.spacy.load("en_core_sci_sm", disable=["parser", "ner"])
+
+        def preprocess(sentence):
+            if lemma:
+                tokens = [token.lemma_ for token in nlp(sentence)
+                          if token not in stopwords]
+            else:
+                tokens = [token for token in sentence.split()
+                          if token not in stopwords]
+            return tokens
+
+        def similarity(sent1, sent2):
+            tokens1 = preprocess(sent1)
+            tokens2 = preprocess(sent2)
+            d2v_model.random.seed(0)
+            return 1 - cosine(d2v_model.infer_vector(tokens1),
+                              d2v_model.infer_vector(tokens2))
+
+        bio_sents = self.get_sentence_df()
+        real_scores = self.get_score_df()
+        d2v_scores = bio_sents.apply(
+            lambda row: similarity(row[0], row[1]), axis=1)
+        avg_real_scores = real_scores.apply(
+            lambda row: sum(row)/len(row), axis=1)
+        corr, _ = pearsonr(avg_real_scores, d2v_scores)
+
+        return corr, d2v_scores
 
 
 class PMCOASubsetCorpus:
@@ -70,11 +105,17 @@ class PMCOASubsetCorpus:
     both usage groups: commercial and non-commercial use.
     Source: https://www.ncbi.nlm.nih.gov/pmc/tools/ftp/.
     """
-    def __init__(self, 
-                 packages=["0-9A-B"], 
-                 size=float("inf"), 
-                 lemma=False, 
-                 stopwords=[], 
+
+    import re
+    import spacy
+    import smart_open
+    from gensim.models.doc2vec import TaggedDocument
+
+    def __init__(self,
+                 packages=["0-9A-B"],
+                 size=float("inf"),
+                 lemma=False,
+                 stopwords=[],
                  sub_pattern=None):
         """Loads user-defined corpus packages.
         User can determine the number of articles from all or any combination 
@@ -93,54 +134,55 @@ class PMCOASubsetCorpus:
         # Could try r"(\s+\(*(([À-ÿA-Za-z\s\-.,;&])+\s\(*(\d{4}[a-z]*)+\)*)+)|(\s+\[[\d\s+,;&\[\]]+\])"
         # for getting rid of majority of in-text citations
         self.corpus_paths = self._load()
-
+        self.nlp = self.spacy.load(
+            name="en_core_sci_sm", disable=["parser", "ner"])
 
     def __iter__(self):
         """Enables users to treat this object as an iterable."""
 
         return self._iterator()
 
-
     def _iterator(self):
         """Creates a generator of preprocessed articles from the PMCOA Subset."""
 
         doc_id = 0
-        while doc_id < self.size:
-            tagged_doc = self._preprocess_doc(self.corpus_paths[doc_id], doc_id)
+        for paper_path in self.corpus_paths:
+            tagged_doc = self._preprocess_doc(paper_path, doc_id)
             if tagged_doc is not None:
                 yield tagged_doc
                 doc_id += 1
+                if doc_id == self.size:
+                    break
 
-                
     def _preprocess_doc(self, path, id):
         """Tokenizes text from a file.
-        
+
         Args:
           path: Filepath of the article to be preprocessed.
           id: Article ID number required for training Doc2Vec.
-        
+
         Returns:
           Tokens in the form of a TaggedDocument,
           following Doc2Vec's training protocol.
         """
 
-        with open(path, "r", encoding="ascii", errors="ignore") as f:
+        with self.smart_open.open(path, "r", encoding="ascii", errors="ignore") as f:
             paper = f.read()
             if self.sub_pattern is not None:
-                paper = re.sub(self.sub_pattern, "", f.read())
-            body = re.search("==== Body(.*)==== Refs", paper, re.DOTALL)
+                paper = self.re.sub(self.sub_pattern, "", paper)
+            body = self.re.search(
+                "==== Body(.*)==== Refs", paper, self.re.DOTALL)
 
             if body is not None:
                 doc = body.group(1).lower().split()
                 if self.lemma:
                     doc = " ".join(doc)
-                    doc = [token.lemma_ for token in self.nlp(doc) \
+                    doc = [token.lemma_ for token in self.nlp(doc)
                            if not token in self.stopwords]
                 else:
-                    doc = [token for token in doc \
+                    doc = [token for token in doc
                            if not token in self.stopwords]
-                return TaggedDocument(doc, [id])
-
+                return self.TaggedDocument(doc, [id])
 
     def _append_corpus_paths(self, start_path, path_arr):
         """Recursively scans and appends paths of all .txt files 
@@ -159,7 +201,6 @@ class PMCOASubsetCorpus:
             if entry.name.endswith(".txt"):
                 path_arr.append(entry.path)
 
-
     def _load(self):
         """Downloads the zipped corpus folders and unzips
         them if they are not already available in the current directory.
@@ -167,28 +208,37 @@ class PMCOASubsetCorpus:
         an internal array, necessary for the iterator later.
         """
 
-        if self.lemma:
-            import spacy        
-            self.nlp = spacy.load(name="en_core_sci_sm", 
-                                  disable=["parser","ner"])
         corpus_paths = []
 
         for package in self.packages:
             zipnames = ["non_comm_use." + package + ".txt.tar.gz",
                         "comm_use." + package + ".txt.tar.gz"]
-            
+
             for zname in zipnames:
                 dirname = zname.replace(".txt.tar.gz", "")
                 if not os.path.isdir(dirname):
                     os.mkdir(dirname)
                     if not os.path.isfile(zname):
-                        run(["wget", 
-                            "ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/" 
-                            + zname])
+                        run(["wget",
+                            "ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/"
+                             + zname])
                     run(["tar", "-xf", zname, "-C", dirname])
                 self._append_corpus_paths(dirname, corpus_paths)
-            
+
         return corpus_paths
+
+    def get_list(self):
+        doc_list = []
+        doc_id = 0
+        for paper_path in self.corpus_paths:
+            tagged_doc = self._preprocess_doc(paper_path, doc_id)
+            if tagged_doc is not None:
+                doc_list.append(tagged_doc)
+                doc_id += 1
+                if doc_id == self.size:
+                    break
+
+        return doc_list
 
 
 class Doc2VecRunner:
@@ -199,48 +249,48 @@ class Doc2VecRunner:
     Doc2Vec's documentation: https://radimrehurek.com/gensim/models/doc2vec.html.
     """
 
+    import logging
+    from gensim.models.doc2vec import Doc2Vec
+
     def __init__(self, corpus=None, **kwargs):
         """Initializes internal model and corpus"""
 
-        self.model = Doc2Vec(**kwargs)
+        self.model = self.Doc2Vec(**kwargs)
         self.corpus = corpus
 
-
-    def run(self, 
-            use_logger=False, 
-            log_dir=None, 
+    def run(self,
+            use_logger=False,
+            log_dir=None,
             progress_per=None):
         """Trains the Doc2Vec model, with optional progress logging"""
 
         if use_logger:
-            import logging
             log_dir = "model" if log_dir is None else log_dir
             log_fname = "{}.log".format(log_dir)
             if not os.path.isdir(log_dir):
                 os.mkdir(log_dir)
             logpath = os.path.join(log_dir, log_fname)
-            logging.basicConfig(format="%(asctime)s : \
-                                        %(levelname)s : \
-                                        %(message)s",
-                                level=logging.INFO,
-                                handlers=[
-                                    logging.FileHandler(logpath, 
-                                                        mode="a"),
-                                    logging.StreamHandler()
-                                ])    
+            self.logging.basicConfig(
+                format="%(asctime)s : %(levelname)s : %(message)s",
+                level=self.logging.INFO,
+                handlers=[
+                    self.logging.FileHandler(filename=logpath,
+                                             mode="w"),
+                    self.logging.StreamHandler()
+                ])
 
         if progress_per is not None:
-            self.model.build_vocab(self.corpus, 
+            self.model.build_vocab(self.corpus,
                                    progress_per=progress_per)
         else:
             self.model.build_vocab(self.corpus)
-        self.model.train(self.corpus, 
-                         total_examples=self.model.corpus_count, 
+        self.model.train(self.corpus,
+                         total_examples=self.model.corpus_count,
                          epochs=self.model.epochs)
-
+        return self.model
 
     def save_model(self, model_dir=None):
-        """Saves model's internal weights to directory"""
+        """Saves model's internal weights to a directory"""
 
         model_dir = "model" if model_dir is None else model_dir
         model_fname = "{}.gsm".format(model_dir)
@@ -249,18 +299,14 @@ class Doc2VecRunner:
         savepath = os.path.join(model_dir, model_fname)
         self.model.save(savepath)
 
-  
-    def get_model(self):
-        return self.model
 
-
-def run_doc2vec(iterator, 
-                lemma, 
-                use_logger, 
-                save_model, 
-                package_names, 
-                corpus_size, 
-                progress_per, 
+def run_doc2vec(iterator,
+                lemma,
+                use_logger,
+                save_model,
+                package_names,
+                corpus_size,
+                progress_per,
                 **kwargs):
     """Program to run based on CLI arguments.
     Helps train a configurable Doc2Vec model on a configurable corpus.
@@ -268,24 +314,31 @@ def run_doc2vec(iterator,
     based on whether lemmatization or data iteration is enabled.
     """
 
-    corpus = PMCOASubsetCorpus(packages=package_names, size=1000, lemma=lemma)
-    corpus = list(corpus) if not iterator else corpus
+    corpus = PMCOASubsetCorpus(
+        packages=package_names, size=corpus_size, lemma=lemma)
+    if not iterator:
+        corpus = corpus.get_list()
 
-    model_dir = "biod2v_{}lemma_{}iter".format("!" if not lemma else "", 
-                                               "!" if not iterator else "")
+    model_dir = "biod2v_{}lemma_{}iter".format(
+        "!" if not lemma else "", "!" if not iterator else "")
     runner = Doc2VecRunner(corpus, **kwargs)
-    runner.run(use_logger, model_dir, progress_per=progress_per)
+    model = runner.run(use_logger, model_dir, progress_per=progress_per)
     if save_model:
         runner.save_model(model_dir)
+    biosses = BIOSSESDataset()
+    correlation, _ = biosses.benchmark_with_d2v(model, lemma)
+    print("Correlation:", correlation)
 
 
 def main():
     """
     Parsing arguments to power Doc2Vec with the PMCOA Subset corpus
     if user wants to use this module in the CLI.
-    """    
-    
+    """
+
     import argparse
+    from multiprocessing import cpu_count
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-i", "--iterator",
@@ -299,15 +352,15 @@ def main():
         help="Whether tokens are turned into lemmas for training."
     )
     parser.add_argument(
-        "-L", "--use-logger",
+        "--use-logger",
         action="store_true",
         help="Whether to show training progress."
     )
     parser.add_argument(
-        "-S", "--save-model",
+        "--save-model",
         action="store_true",
         help="Whether to save the model weights."
-    )    
+    )
     parser.add_argument(
         "-n", "--package-names",
         type=str,
@@ -329,7 +382,7 @@ def main():
     parser.add_argument(
         "-v", "--vector-size",
         type=int,
-        default=56,
+        default=100,
         help="Dimensionality of Doc2Vec embeddings."
     )
     parser.add_argument(
@@ -344,7 +397,7 @@ def main():
         type=float,
         default=0.025,
         help="Initial learning rate."
-    )    
+    )
     parser.add_argument(
         "-m", "--min-count",
         type=int,
@@ -354,7 +407,7 @@ def main():
     parser.add_argument(
         "-W", "--workers",
         type=int,
-        default=3,
+        default=cpu_count(),
         help="Number of worker threads used to train the model."
     )
     parser.add_argument(
@@ -365,16 +418,16 @@ def main():
     )
     args, _ = parser.parse_known_args()
     run_doc2vec(iterator=args.iterator,
-                lemma=args.lemma, 
-                use_logger=args.use_logger, 
+                lemma=args.lemma,
+                use_logger=args.use_logger,
                 save_model=args.save_model,
                 package_names=args.package_names,
                 corpus_size=args.corpus_size,
                 progress_per=args.progress_per,
-                vector_size=args.vector_size, 
-                window=args.window, 
+                vector_size=args.vector_size,
+                window=args.window,
                 alpha=args.alpha,
-                min_count=args.min_count, 
+                min_count=args.min_count,
                 workers=args.workers,
                 epochs=args.epochs)
 
